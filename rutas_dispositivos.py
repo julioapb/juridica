1205293915374fee0c2e2b8c24004334fb4ec850
julioapb <juliopierosbautista@gmail.com>
2025-04-11 02:51:19 +0200

    Guardar nuevo dispositivo

--- rutas_dispositivos.py
+++ rutas_dispositivos.py
@@ -25,10 +25,61 @@ def teardown_db(exception):
 
 @dispositivos.route('/nuevoDispositivo')
 def nuevoDispositivo():
+    
+    if 'username' not in session:
+        return redirect(url_for('auth.login'))
+
+    username = session['username']
+
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM tipo_dispositivo ORDER BY dispositivo ASC")
+    tipo_dispositivos = cursor.fetchall()
+
+    cursor.execute("SELECT * FROM estado_dispositivo ORDER BY estado ASC")
+    estados_dispositivo = cursor.fetchall()
+
+    cursor.execute("SELECT * FROM estado_asignacion ORDER BY estado ASC")
+    estados_asignacion = cursor.fetchall()
+
+    return render_template('vista_dispositivos.html', username = username, tipo_dispositivos = tipo_dispositivos, estados_dispositivo = estados_dispositivo, estados_asignacion = estados_asignacion)
 
 
-    return render_template('vista_dispositivos.html')
+@dispositivos.route('/guardarDispositivo', methods = ['POST'])
+def guardarDsipositivo():
 
+    tipo_dispositivo = request.form['tipo_dispositivo'].upper()
+    no_serie = request.form['no_serie']
+    marca = request.form['marca'].upper()
+    modelo = request.form['modelo'].upper()
+    color = request.form['color'].upper()
+    disco = request.form['disco']
+    ram = request.form['ram']
+    procesador = request.form['procesador'].upper()
+    sistema_operativo = request.form['sistema_operativo'].upper()
+    fecha_ingreso = request.form['fecha_ingreso']
+    estado_asignacion = request.form['estado_asignacion']
+    estado_dispositivo = request.form['id_estado_dispositivo']
 
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM dispositivo WHERE no_serie = %s", (no_serie,))
+    dispositivo_existente = cursor.fetchone()
 
+    if dispositivo_existente:
+        flash('El dispositivo ya está registrado', 'danger')
+        cursor.close()
+        return redirect(url_for('dispositivos.nuevoDispositivo'))
+    
+    cursor.execute("""
+        INSERT INTO dispositivo(no_serie, marca, id_tipo_dispositivo, modelo, color, disco, ram, procesador, sistema_operativo, fecha_ingreso, 
+                   id_estado_dispositivo, id_estado_asignacion)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """,(no_serie, marca, tipo_dispositivo, modelo, color, disco, ram, procesador, sistema_operativo, fecha_ingreso, estado_dispositivo, estado_asignacion  )
+    )
+
+    db.commit()
+    cursor.close()
+    flash('✅ Dsipositivo registrado correctamente.', 'success')
+    return redirect(url_for('dispositivos.nuevoDsipositivo'))
 
